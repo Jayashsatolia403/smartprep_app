12c4d3cab66a20244b8f35dac85be4f6a67f7093
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2021-12-03 23:56:42 +0530

    Update

--- Backend/Apps/Questions/admin.py
+++ Backend/Apps/Questions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import DailyQuestions, Exams, Options, PracticeQuestions, Questions, Subjects, QuestionsOfTheDays, PrevQuesOfDays
+from .models import DailyQuestions, Exams, Options, PracticeQuestions, QuestionBookmarks, Questions, Subjects, QuestionsOfTheDays, PrevQuesOfDays
 
 admin.site.register(Questions)
 admin.site.register(Options)
@@ -10,3 +10,4 @@ admin.site.register(QuestionsOfTheDays)
 admin.site.register(PrevQuesOfDays)
 admin.site.register(DailyQuestions)
 admin.site.register(PracticeQuestions)
+admin.site.register(QuestionBookmarks)
