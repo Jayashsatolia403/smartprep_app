915c58b00e43342374f1498ad7fd3b1a5b565d53
Jayashsatolia403 <73050737+Jayashsatolia403@users.noreply.github.com>
2021-12-19 20:43:21 +0530

    Update views.py

--- Backend/Apps/Chat/views.py
+++ Backend/Apps/Chat/views.py
@@ -48,7 +48,7 @@ def getAllForumMessages(request):
 
 @api_view(['POST', ])
 def sendForumMessage(request):
-    try:
+#     try:
         serializer = ForumMessageSerializer(data=request.data, context={'request': request})
 
         forumName = request.POST['forum']
@@ -63,8 +63,8 @@ def sendForumMessage(request):
             return Response("Success")
         else:
             return "Oops"
-    except:
-        return Response("Invalid Request")
+#     except:
+#         return Response("Invalid Request")
     
 
 
